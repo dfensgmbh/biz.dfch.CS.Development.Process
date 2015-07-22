1da5121118083603aedd185c6559df410ee8d8f2
rufer7 <marc.rufer@d-fens.net>
2015-07-22 18:01:44 +0200

    resolves #4

--- biz.dfch.CS.Development.Process/biz.dfch.CS.Development.Process.Tests/CalculatorTest.cs
+++ biz.dfch.CS.Development.Process/biz.dfch.CS.Development.Process.Tests/CalculatorTest.cs
@@ -11,5 +11,11 @@ namespace biz.dfch.CS.Development.Process.Tests
         {
             Assert.AreEqual(15, Calculator.add(7, 8));
         }
+
+        [TestMethod]
+        public void SubractReturnsDifferenceOfTheTwoPassedNumbers()
+        {
+            Assert.AreEqual(2, Calculator.subtract(10, 8));
+        }
     }
 }
